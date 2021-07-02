ef16aba48baf0afcaae9e9c55a0a2d0e82f4bbd9
Emi <e.wong@hotmail.co.uk>
2021-07-02 12:57:42 +0100

    add initial board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Game from './components/Game';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Game />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Noughts and Crosses</h1>
+    </header>
+  );
+};
+
+export default Header;
